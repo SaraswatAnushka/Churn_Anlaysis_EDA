70d9a4c68d146625427c31551e38d1dfac54c4da
SaraswatAnushka <saraswatanushka007@gmail.com>
2024-03-03 19:51:07 +0530

    changes in app file

--- churn_prediction/app.py
+++ churn_prediction/app.py
@@ -1,19 +1,29 @@
-import pandas as pd 
+
+# coding: utf-8
+
+import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier 
 from sklearn import metrics
-from flask import Flask , request , render_template
+from flask import Flask, request, render_template
 import pickle
 
 app = Flask("__name__")
 
-df1 = pd.read_csv("tel_churn.csv")
+df_1=pd.read_csv("first_telc.csv")
+df_1= df_1.drop("Unnamed: 0", axis = 1)
+
+
 q = ""
+
 @app.route("/")
-def laodPage():
-    return render_template('home.html',query = "")
+def loadPage():
+	return render_template('index.html', query="")
+
+
 @app.route("/", methods=['POST'])
 def predict():
+    
     '''
     SeniorCitizen
     MonthlyCharges
@@ -35,6 +45,9 @@ def predict():
     PaymentMethod
     tenure
     '''
+    
+
+    
     inputQuery1 = request.form['query1']
     inputQuery2 = request.form['query2']
     inputQuery3 = request.form['query3']
@@ -54,40 +67,54 @@ def predict():
     inputQuery17 = request.form['query17']
     inputQuery18 = request.form['query18']
     inputQuery19 = request.form['query19']
-   
-    model = pickle.load(open("model.sav","rb"))
+
+    model = pickle.load(open("model.sav", "rb"))
+    
     data = [[inputQuery1, inputQuery2, inputQuery3, inputQuery4, inputQuery5, inputQuery6, inputQuery7, 
-            inputQuery8, inputQuery9, inputQuery10, inputQuery11, inputQuery12, inputQuery13, inputQuery14,
-            inputQuery15, inputQuery16, inputQuery17, inputQuery18, inputQuery19]]
+             inputQuery8, inputQuery9, inputQuery10, inputQuery11, inputQuery12, inputQuery13, inputQuery14,
+             inputQuery15, inputQuery16, inputQuery17, inputQuery18, inputQuery19]]
     
-    new_df = pd.Dataframe(data,columns = ['SeniorCtizen', 'MonthlyCharges', 'TotalCharges','gender', 'Partner','Depemdents','PhoneService','MultipleLines',
-                                          'InternetService','OnlineSecurity','OnlineBackup','DeviceProtection','TechSupport','StreamingTV','StreamingMovies'
-                                          ,'Contract','PaperlessBilling','PaymentMethod','tenure'])
+    new_df = pd.DataFrame(data, columns = ['SeniorCitizen', 'MonthlyCharges', 'TotalCharges', 'gender', 
+                                           'Partner', 'Dependents', 'PhoneService', 'MultipleLines', 'InternetService',
+                                           'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport',
+                                           'StreamingTV', 'StreamingMovies', 'Contract', 'PaperlessBilling',
+                                           'PaymentMethod', 'tenure'])
     
-    df_2 = pd.concat([df_1,new_df],ignore_index=True)
-    #Label the tenure in bins of 12 months
-
-    labels = ["{0}-{1}".format(i,i+1) for i in range(1,72,12)]
+    df_2 = pd.concat([df_1, new_df], ignore_index = True) 
+    df_2['tenure'] = pd.to_numeric(df_2['tenure'], errors='coerce')
+    df_2['tenure'].fillna(-1, inplace=True)
+    # Group the tenure in bins of 12 months
+    labels = ["{0} - {1}".format(i, i + 11) for i in range(1, 72, 12)]
+    
     df_2['tenure_group'] = pd.cut(df_2.tenure.astype(int), range(1, 80, 12), right=False, labels=labels)
-    # drop column customerID and tenure
-    df_2.drop(columns = ['tenure'], axis= 1, inplace = True)
-
+    #drop column customerID and tenure
+    df_2.drop(columns= ['tenure'], axis=1, inplace=True)   
+    
+    
+    
+    
     new_df__dummies = pd.get_dummies(df_2[['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'PhoneService',
            'MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup',
            'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies',
            'Contract', 'PaperlessBilling', 'PaymentMethod','tenure_group']])
     
+    # print("shape of new_dummies",new_df__dummies.shape)
+    
+    
+    #final_df=pd.concat([new_df__dummies, new_dummy], axis=1)
+        
+    
     single = model.predict(new_df__dummies.tail(1))
-    probability = model.predict_proba(new_df__dummies.tail(1))[:,1]
-
+    probablity = model.predict_proba(new_df__dummies.tail(1))[:,1]
+    
     if single==1:
         o1 = "This customer is likely to be churned!!"
         o2 = "Confidence: {}".format(probablity*100)
     else:
         o1 = "This customer is likely to continue!!"
-        o2 = "Confidence: {}".format(probablity*100)    
-
-    return render_template('home.html', output1=o1, output2=o2, 
+        o2 = "Confidence: {}".format(probablity*100)
+        
+    return render_template('index.html', output1=o1, output2=o2, 
                            query1 = request.form['query1'], 
                            query2 = request.form['query2'],
                            query3 = request.form['query3'],
@@ -108,5 +135,5 @@ def predict():
                            query18 = request.form['query18'], 
                            query19 = request.form['query19'])
 
-app.run()
-     
+
+app.run(port=5002, debug=True)
